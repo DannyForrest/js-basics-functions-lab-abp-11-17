9efe3bbc1a89b45cf084a50941cca450449e3dee
Danny Forrest <dannyforrest@gmail.com>
2017-11-09 22:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution in this file!
 function calculateVertical(start, end) {
-  return (start-end)*264;
+  return Math.abs(start-end)*264;
 }
 
 function distanceFromHqInBlocks(pickup) {
   return Math.abs(42-pickup);
+}
+
+function distanceFromHqInFeet(pickup) {
+  return calculateVertical(pickup, 42);
 }
